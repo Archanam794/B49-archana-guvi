638d3733440bf6f1c42e63836bd412f56bdc090b
Archanam794 <archanam794@gmail.com>
2023-08-09 19:47:41 +0530

    new change

--- React-FrontEnd/Day-2/my-app/src/App.jsx
+++ React-FrontEnd/Day-2/my-app/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <div>
         my react
       </div>
+      
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
