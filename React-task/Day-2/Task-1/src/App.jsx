49a11bd3d4fbfaa8ab144183ecfebfb52ca210ab
Archanam794 <archanam794@gmail.com>
2023-08-14 20:05:11 +0530

    task-1 react

---
+++ React-task/Day-2/Task-1/src/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  render() {
+    return (
+      React.createElement('div',{className:"wrapper"},React.createElement('div',{className:"table basic"},React.createElement('div',{className:"price-section"},React.createElement('div',{className:"price-area"},React.createElement('div',
+      {className:"inner-area"},React.createElement('span',{className:"text"},"Rs."),React.createElement('span',{className:"price"},"00")))),React.createElement('div',{className:"package-name"}, null),
+      React.createElement('div',{className:"features"},React.createElement('li',null, React.createElement('span',{className:"list-name"},"One Selected Template"),React.createElement('span',{className:"icon check"},React.createElement('i',{className:"fas fa-check-circle"},null)))
+      ,React.createElement('li',null,React.createElement('span',{className:"list-name"},"100% Responsive Desgin"),
+      React.createElement('span',{className:"icon check"},React.createElement('i',{className:"fas fa-check-circle"},null))),
+      React.createElement('li',null,React.createElement('span',{className:"list-name"},"Credit Remove Permission"),
+      React.createElement('span',{className:"icon cross"},React.createElement('i',{className:"far fa-times-circle"},null))),
+      React.createElement('li',null,React.createElement('span',{className:"list-name"},"Lifetime Template Updates"),
+      React.createElement('span',{className:"icon cross"},React.createElement('i',{className:"far fa-times-circle"},null))),
+      React.createElement('div',{className:"btn"},React.createElement('button',null,"Purchase"))
+      )),React.createElement('div',{className:"table Premium"},React.createElement('div',{className:"price-section"},React.createElement('div',{className:"price-area"},React.createElement('div',
+      {className:"inner-area"},React.createElement('span',{className:"text"},"Rs."),React.createElement('span',{className:"price"},"49")))),React.createElement('div',{className:"package-name"}, null),
+      React.createElement('div',{className:"features"},React.createElement('li',null, React.createElement('span',{className:"list-name"},"Five Existing Template"),React.createElement('span',{className:"icon check"},React.createElement('i',{className:"fas fa-check-circle"},null)))
+      ,React.createElement('li',null,React.createElement('span',{className:"list-name"},"100% Responsive Desgin"),
+      React.createElement('span',{className:"icon check"},React.createElement('i',{className:"fas fa-check-circle"},null))),
+      React.createElement('li',null,React.createElement('span',{className:"list-name"},"Credit Remove Permission"),
+      React.createElement('span',{className:"icon cross"},React.createElement('i',{className:"far fa-times-circle"},null))),
+      React.createElement('li',null,React.createElement('span',{className:"list-name"},"Lifetime Template Updates"),
+      React.createElement('span',{className:"icon cross"},React.createElement('i',{className:"far fa-times-circle"},null))),
+      React.createElement('div',{className:"btn"},React.createElement('button',null,"Purchase"))
+      )),
+      React.createElement('div',{className:"table Ultimate"},React.createElement('div',{className:"price-section"},React.createElement('div',{className:"price-area"},React.createElement('div',
+      {className:"inner-area"},React.createElement('span',{className:"text"},"Rs."),React.createElement('span',{className:"price"},"99")))),React.createElement('div',{className:"package-name"}, null),
+      React.createElement('div',{className:"features"},React.createElement('li',null, React.createElement('span',{className:"list-name"},"All Existing Template"),React.createElement('span',{className:"icon check"},React.createElement('i',{className:"fas fa-check-circle"},null)))
+      ,React.createElement('li',null,React.createElement('span',{className:"list-name"},"100% Responsive Desgin"),
+      React.createElement('span',{className:"icon check"},React.createElement('i',{className:"fas fa-check-circle"},null))),
+      React.createElement('li',null,React.createElement('span',{className:"list-name"},"Credit Remove Permission"),
+      React.createElement('span',{className:"icon cross"},React.createElement('i',{className:"far fa-times-circle"},null))),
+      React.createElement('li',null,React.createElement('span',{className:"list-name"},"Lifetime Template Updates"),
+      React.createElement('span',{className:"icon cross"},React.createElement('i',{className:"far fa-times-circle"},null))),
+      React.createElement('div',{className:"btn"},React.createElement('button',null,"Purchase"))
+      ))
+      )
+    )
+  }
+}
+
+      
+    
+  
+
+
+    
